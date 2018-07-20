0c8a6fb3aa47cc89dc37b11017d1f3df44361a04
Karen Posada <k100posada@gmail.com>
2018-07-20 18:50:26 -0500

    adds front-end ipfs folder

---
+++ ipfs-trive-news/src/storehash.js
@@ -0,0 +1,37 @@
+import web3 from './web3';
+
+//access our local copy to contract deployed on rinkeby testnet
+
+const address = '0x66863d08547012dc267d2f871b20f68110262f66';
+//ABI from your contract
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+export default new web3.eth.Contract(abi, address);
