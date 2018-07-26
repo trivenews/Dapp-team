51cb96a72c43cee3dd9b04530f7e2cc4a540d3ed
Chance Baars <chance@Chance.home>
2018-07-26 17:41:12 -0400

    usercrud.sol is working, usertask.sol only needs the verify task function

--- ipfs-trive-news/contracts/UserCrud.sol
+++ ipfs-trive-news/contracts/UserCrud.sol
@@ -1,40 +1,37 @@
-pragma solidity ^0.4.19;
+// not deployed
+
+pragma solidity ^0.4.24;
 
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+contract UserCreation {
 
-contract UserCrud {
-
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
 	event NewUser(address user, uint id, string username);
-
-	uint cooldownTime = 2 hours;
 
 	struct User {
 	    string name;
 	    uint reputation;
 	    uint articleCount;
-	    uint panaltyCount;
+	    uint penaltyCount;
 	    uint readyTime;
 	    bool busyResearcher;
+	    uint researchedArticlesCount;
 	}
 
 	User[] public users;
 	uint totalUserCount = 0;
-	
-	//count the users linked to given address
+
 	mapping (address => uint) public ownerUserCount;
-	//from address to User id
 	mapping (address => uint) public findUserId;
-	//check address if it's a researcher
 	mapping (address => bool) public isResearcher;
 
-	function createUser(string _name) public {
+	function _createUser(string _name) public {
 	    //check if owner already has an account
 	    require(ownerUserCount[msg.sender] == 0);
 
 	    //create new User
-	    uint id = users.push(User(_name, 0, 0, 0, uint32(now), false)).sub(1);
+	    uint id = users.push(User(_name, 0, 0, 0, uint32(now), false, 0)).sub(1);
 
 	    //add user to owner to user count mapping
 	    ownerUserCount[msg.sender] = ownerUserCount[msg.sender].add(1);
@@ -50,20 +47,27 @@ contract UserCrud {
 	    emit NewUser(msg.sender, id, _name);
 	}
 
-	function findUserInfo() public view returns(string, uint, uint, uint, uint, bool){
+	// test function to create a researcher
+	function _createResearcher() public {
+	    isResearcher[msg.sender] = true;
+	}
+	// end test funtion
+
+	function _findUserInfo() public view returns(string, uint, uint, uint, uint, bool){
 	    // So you can only request you own info
 	    require(ownerUserCount[msg.sender] == 1);
 	    //look up id
 	    uint id = findUserId[msg.sender];
 	    //return requested info
-	    return (users[id].name, users[id].reputation, users[id].articleCount, users[id].panaltyCount, users[id].readyTime, users[id].busyResearcher);
+	    return (users[id].name, users[id].reputation, users[id].articleCount, users[id].penaltyCount, users[id].readyTime, users[id].busyResearcher);
 	}
 
-	function getToatalUserCount() public view returns(uint) {
+	function _getToatalUserCount() public view returns(uint) {
 	    return totalUserCount;
 	}
 
 }
+
 
 
 /* pragma solidity ^0.4.24;
--- ipfs-trive-news/contracts/UserTask.sol
+++ ipfs-trive-news/contracts/UserTask.sol
@@ -1,6 +1,106 @@
 // not deployed
 
-pragma solidity ^0.4.19;
+
+pragma solidity ^0.4.24;
+
+import "./UserCrud.sol";
+
+contract TaskAndReputation is UserCreation {
+    using SafeMath for uint256;
+    event  NewTask(address creator, uint reward);
+
+    enum State { Open, InProgress, Solved, Verified }
+
+    struct Task {
+        string	 IPFShash;
+        string   IPFShashResearch;
+    		uint	   reward;
+    		State    state;
+    }
+
+    Task[] public tasks;
+    uint cooldownTime = 2 hours;
+    uint totalTasksCount = 0;
+
+    // mapping (uint => uint) public lookupTasksByGenre;
+
+    // from creator address to taskid find article count from user.articleCount
+    mapping (address => uint) public ownerTaskCount;
+    mapping (uint => address) public taskToOwner;
+    // researcher of the task
+    mapping (uint => address) public taskToResearcher;
+
+
+
+    function _changeUserInfo(User storage _user) private {
+        _user.articleCount = _user.articleCount.add(1);
+        _user.reputation = (_user.articleCount.add(_user.researchedArticlesCount)).sub(_user.penaltyCount);
+    }
+    // maybe add an "is user" modifier
+    function _createTask(string _ipfsHash, uint _reward) public payable {
+        /// later we will have to add a minimum value from the msger.
+        //create new task id and push to tasks array and taskToOwner
+        uint taskId = tasks.push(Task(_ipfsHash, "", _reward, false, State.Open)).sub(1);
+        taskToOwner[taskId] = msg.sender;
+
+	    //add task to ownerTaskCount
+	    ownerTaskCount[msg.sender] = ownerTaskCount[msg.sender].add(1);
+
+	    //change user reputation
+	    uint userId = findUserId[msg.sender];
+	    User storage me = users[userId];
+	    _changeUserInfo(me);
+
+	    totalTasksCount = totalTasksCount.add(1);
+
+	    // fire event
+	    emit NewTask(msg.sender, _reward);
+    }
+
+    //researcher accepts
+    function _acceptTask(uint _taskId) public {
+        //check if accepter is a researcher
+        require(isResearcher[msg.sender] == true);
+        // set mapping taskId to researcher
+        taskToResearcher[_taskId] = msg.sender;
+        // change state of the task to InProgress
+        Task storage t = tasks[_taskId];
+        t.state = State.InProgress;
+    }
+    // researcher submits
+    function _submitTask(uint _taskId, string _IPFShashResearch) public {
+        //check if submitter is the researcher linked to the task
+        require(taskToResearcher[_taskId] == msg.sender);
+        // change state of the task to solved and add ipfs hash
+        Task storage t = tasks[_taskId];
+        t.state = State.Solved;
+        t.IPFShashResearch = _IPFShashResearch;
+    }
+
+    //get total amount of tasks
+    function getTotalTasksCount() public view returns(uint) {
+        return totalTasksCount;
+    }
+    // so a user can easaly see the answers to the tasks he/she/it created
+    function _getTasksByOwner(address _owner) external view returns(uint[]) {
+        //declaration of short term array
+        uint[] memory result = new uint[](ownerTaskCount[_owner]);
+        uint counter = 0;
+        for (uint i = 0; i < tasks.length; i++) {
+          if (taskToOwner[i] == _owner) {
+            result[counter] = i;
+            counter++;
+          }
+        }
+        return result;
+    }
+
+
+}
+
+
+
+/* pragma solidity ^0.4.19;
 
 import "./UserCrud.sol";
 
@@ -22,4 +122,4 @@ contract UserTask is UserCrud {
         uint taskId = tasks.push(Task(_ipfsHash, _reward));
     }
 
-}
+} */
