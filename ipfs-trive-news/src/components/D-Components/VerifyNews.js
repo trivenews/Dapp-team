0ab1e362d6b678da84aacc64333c0f9699df62ee
Chance Baars <chance@Chance.home>
2018-07-31 17:19:44 -0400

    added research info component and verify functionality

---
+++ ipfs-trive-news/src/components/D-Components/VerifyNews.js
@@ -0,0 +1,79 @@
+import React, {Component} from "react";
+import contract from 'truffle-contract';
+import web3 from '../../web3';
+import ResearchedNewsInfo from "../showComponents/ResearchedNewsInfo";
+
+
+class VerifyNews extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      articleIds: [],
+      articles: []
+    }
+    this.getTaskByState = this.getTaskByState.bind(this);
+    this.findArticleInfo = this.findArticleInfo.bind(this);
+  }
+
+  getTaskInfo(articleId) {
+    const TriveDapp = this.props.myContract;
+    var TriveDappInstance;
+    TriveDapp.deployed().then((instance) => {
+      TriveDappInstance = instance;
+      return TriveDappInstance._getTaskInfo(articleId)
+    }).then((result) => {
+      console.log(result)
+      var articles = [...this.state.articles];
+      articles.push(<ResearchedNewsInfo myContract={this.props.myContract} articleId={articleId} key={articleId} data={result} curAddr={this.props.curAddr}/>);
+
+      this.setState({
+        articles
+      });
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  findArticleInfo(arr) {
+    let res = [];
+    arr.map(num => {
+      res.push(parseInt(num.toString()))
+    });
+    // console.log(res);
+    this.setState({
+      articleIds: res
+    });
+
+    res.forEach((num) => {
+      this.getTaskInfo(num);
+    });
+  }
+
+  getTaskByState() {
+    const TriveDapp = this.props.myContract;
+    var TriveDappInstance;
+    TriveDapp.deployed().then((instance) => {
+      TriveDappInstance = instance;
+      return TriveDappInstance._getTasksByState(2, 2)
+    }).then((result) => {
+      this.findArticleInfo(result);
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  componentDidMount() {
+    this.getTaskByState();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Verify NEWS</h1>
+        <hr />
+        {this.state.articles}
+      </div>
+    )
+  }
+}
+
+export default VerifyNews;
