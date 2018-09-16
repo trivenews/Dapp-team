f516b2938833256237b8a2dfd0eaa9e8b7612e0d
Karen Posada <k100posada@gmail.com>
2018-09-16 18:37:36 -0500

    updating css for the dashboard and form sections

--- src/components/D-Components/SubmitArticle.js
+++ src/components/D-Components/SubmitArticle.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import {Table, Grid, Button, Form } from 'react-bootstrap';
-import { withRouter } from 'react-router-dom';
-import {bindActionCreators} from 'redux';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { Table, Grid, Button, Form } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 // import contract from 'truffle-contract';
 
-
-import web3 from '../../web3';
-import { setJSON } from '../../util/IPFS.js';
-import Loader from './Loader';
+import web3 from "../../web3";
+import { setJSON } from "../../util/IPFS.js";
+import Loader from "./Loader";
 // import storehash from '../../storehash';
 
 class Verify extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      ipfsHash:null,
-      buffer:'',
-      ethAddress:'',
-      transactionHash:'',
-      txReceipt: '',
+      ipfsHash: null,
+      buffer: "",
+      ethAddress: "",
+      transactionHash: "",
+      txReceipt: "",
       loading: false,
       myData: {
         url: "",
@@ -27,50 +26,50 @@ class Verify extends Component {
         description: "",
         image: null
       },
-      reward: ''
+      reward: ""
     };
   }
 
-
-  handleMyData = (e) => {
-    if (e.key === 'Enter') {
-      console.log('do validate');
+  handleMyData = e => {
+    if (e.key === "Enter") {
+      console.log("do validate");
     }
-    console.log(this.state.myData)
+    console.log(this.state.myData);
     const { name, value } = e.target;
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       myData: {
         ...prevState.myData,
         [name]: value
       }
     }));
-  }
+  };
 
-  handleMyDataReward = (e) => {
+  handleMyDataReward = e => {
     this.setState({ reward: e.target.value });
-  }
+  };
 
-  captureFile = (event) => {
-    event.preventDefault()
-    const file = event.target.files[0]
-    const reader = new window.FileReader()
-    reader.readAsArrayBuffer(file)
+  captureFile = event => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
     reader.onloadend = () => {
-      this.setState((prevState) => ({
+      this.setState(prevState => ({
         myData: {
           ...prevState.myData,
-          image: `data:image/jpeg;base64,${Buffer(reader.result).toString('base64')}`
+          image: `data:image/jpeg;base64,${Buffer(reader.result).toString(
+            "base64"
+          )}`
         }
-      }))
-    }
-  }
+      }));
+    };
+  };
 
-
-  onSubmit = async (event) => {
+  onSubmit = async event => {
     event.preventDefault();
 
     const { url } = this.state.myData;
-    this.setState({loading: true});
+    this.setState({ loading: true });
     //obtain contract address from storehash.js
     // const ethAddress= await TriveDapp.options.address;
     // this.setState({ethAddress});
@@ -81,30 +80,33 @@ class Verify extends Component {
     //https://github.com/ipfs/interface-ipfs-core/blob/master/SPEC/FILES.md#add
 
     this.setState({ ipfsHash: hash });
-    console.log(hash)
+    console.log(hash);
 
     // call Ethereum contract method "createTask" and send info to etheruem contract
     //return the transaction hash from the ethereum contract
     //see, this https://web3js.readthedocs.io/en/1.0/web3-eth-contract.html#methods-mymethod-send
-    this.props.trive.triveContract.createTask(this.state.ipfsHash, url, this.state.reward * 10 ** 18, {from: this.props.account, gas: 6654755})
-    .then((result) => {
-      console.log(result.tx);
-      this.setState({transactionHash: result.tx, loading: false})
-    }).catch((error) => {
-      // TODO: make an error screen if user didn't approve contract to send tokens
-      this.setState({loading: false})
-      console.log(error);
-    })
-
+    this.props.trive.triveContract
+      .createTask(this.state.ipfsHash, url, this.state.reward * 10 ** 18, {
+        from: this.props.account,
+        gas: 6654755
+      })
+      .then(result => {
+        console.log(result.tx);
+        this.setState({ transactionHash: result.tx, loading: false });
+      })
+      .catch(error => {
+        // TODO: make an error screen if user didn't approve contract to send tokens
+        this.setState({ loading: false });
+        console.log(error);
+      });
   }; //onSubmit
 
   render() {
     ///CHANCE added image to this.state.myData
-    const { title, url, description, image} = this.state.myData
+    const { title, url, description, image } = this.state.myData;
 
     //CHANCE this is the image base64
-    console.log("I'mm I workimg", image)
-
+    console.log("I'mm I workimg", image);
 
     return (
       <div>
@@ -112,73 +114,68 @@ class Verify extends Component {
 
         <Grid className="verify-container">
           <h3> Give Article information to verify and send to IPFS </h3>
-          <img src={image}  className='showImage'/>
+          <img src={image} className="showImage" />
           <br />
-          <Form onSubmit={this.onSubmit} >
-
-            URL
+          <Form onSubmit={this.onSubmit}>
             <br />
             <input
-              type = "url"
+              type="url"
               value={url}
               name="url"
+              placeholder="Please enter the URL in question"
               onChange={this.handleMyData}
-              style={{width: '70%', height: '3em', marginRight: '5px'}}
-                />
-
+              style={{ width: "70%", marginRight: "5px" }}
+            />
             <br />
-            Title
             <br />
             <input
-              type = "text"
+              type="text"
               value={title}
               name="title"
+              placeholder="Please enter the Title of this task"
               onChange={this.handleMyData}
-              style={{width: '70%', height: '3em', marginRight: '5px'}}
-                />
-
-            <br />
-            Image
-            <br />
-            <input
-              type = "file"
-              onChange={this.captureFile}
-                />
-
+              style={{ width: "70%", marginRight: "5px" }}
+            />
             <br />
 
-            Description
+            <br />
+            <div class="upload-btn-wrapper">
+              <button class="upload">Upload an image</button>
+              <input type="file" onChange={this.captureFile} />
+            </div>
+            <br />
             <br />
             <textarea
-              type = "textarea"
+              type="textarea"
               value={description}
               name="description"
+              placeholder="Please enter a detailed description"
               onChange={this.handleMyData}
-              style={{width: '70%', minHeight: '6em', height: 'auto', marginRight: '5px'}}
-                ></textarea>
+              style={{
+                width: "70%",
+                minHeight: "6em",
+                height: "auto",
+                marginRight: "5px"
+              }}
+            />
             <br />
-            Reward
             <br />
             <input
-              type = "number"
+              type="number"
               value={this.state.reward}
               onChange={this.handleMyDataReward}
-              style={{width: '70%', height: '3em', marginRight: '5px'}}
-                />
-
+              placeholder="Please enter the reward offered"
+              style={{ width: "70%", marginRight: "5px" }}
+            />
             <br />
             <br />
-            <Button
-              bsStyle="primary"
-              type="submit">
+            <Button bsStyle="primary" type="submit">
               Send it
             </Button>
-
             <br />
-
           </Form>
 
-          <hr/>
+          <hr />
 
           <Table bordered responsive>
             <thead>
@@ -210,13 +207,13 @@ class Verify extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-	return ({
+const mapStateToProps = state => {
+  return {
     curUserInfo: state.currentUserInfo.curUserInfo,
     account: state.trive.account,
     trive: state.trive.contracts
-  //activeAccount: state.web3.activeAccount
-})
+    //activeAccount: state.web3.activeAccount
+  };
 };
 
 export default withRouter(connect(mapStateToProps)(Verify));
